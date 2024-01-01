67deb6285cbd08132ecb88be1105fcee159e4d38
Tommylee1013 <junghun1013@icloud.com>
2024-01-01 18:53:36 +0900

    Saving

---
+++ week16 Causal Machine Learning/feature_importances.py
@@ -0,0 +1,152 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import log_loss
+from sklearn.model_selection import KFold
+from sklearn.base import ClassifierMixin
+from sklearn.model_selection import BaseCrossValidator
+
+from typing import Callable
+
+class PurgedKFold(KFold):
+    def __init__(self,
+                 n_splits: int = 3,
+                 samples_info_sets: pd.Series = None,
+                 pct_embargo: float = 0.):
+
+        if not isinstance(samples_info_sets, pd.Series):
+            raise ValueError('The samples_info_sets param must be a pd.Series')
+        super(PurgedKFold, self).__init__(n_splits, shuffle=False, random_state=None)
+
+        self.samples_info_sets = samples_info_sets
+        self.pct_embargo = pct_embargo
+    def split(self,
+              X: pd.DataFrame,
+              y: pd.Series = None,
+              groups = None):
+        if X.shape[0] != self.samples_info_sets.shape[0]:
+            raise ValueError("X and the 'samples_info_sets' series param must be the same length")
+
+        indices: np.ndarray = np.arange(X.shape[0])
+        embargo: int = int(X.shape[0] * self.pct_embargo)
+
+        test_ranges: [(int, int)] = [(ix[0], ix[-1] + 1) for ix in np.array_split(np.arange(X.shape[0]), self.n_splits)]
+        for start_ix, end_ix in test_ranges:
+            test_indices = indices[start_ix:end_ix]
+
+            if end_ix < X.shape[0]:
+                end_ix += embargo
+
+            test_times = pd.Series(index=[self.samples_info_sets[start_ix]], data=[self.samples_info_sets[end_ix-1]])
+            train_times = ml_get_train_times(self.samples_info_sets, test_times)
+
+            train_indices = []
+            for train_ix in train_times.index:
+                train_indices.append(self.samples_info_sets.index.get_loc(train_ix))
+            yield np.array(train_indices), test_indices
+
+def ml_get_train_times(samples_info_sets: pd.Series, test_times: pd.Series) -> pd.Series:
+    train = samples_info_sets.copy(deep=True)
+    for start_ix, end_ix in test_times.items():
+        df0 = train[(start_ix <= train.index) & (train.index <= end_ix)].index
+        df1 = train[(train.index <= start_ix) & (end_ix <= train)].index
+        train = train.drop(df0.union(df1))
+    return train
+
+def ml_cross_val_score(
+        classifier: ClassifierMixin,
+        X: pd.DataFrame,
+        y: pd.Series,
+        cv_gen: BaseCrossValidator,
+        sample_weight: np.ndarray = None,
+        scoring: Callable[[np.array, np.array], float] = log_loss):
+    if sample_weight is None:
+        sample_weight = np.ones((X.shape[0],))
+    ret_scores = []
+    for train, test in cv_gen.split(X=X, y=y):
+        fit = classifier.fit(X=X.iloc[train, :], y=y.iloc[train], sample_weight=sample_weight[train])
+        if scoring == log_loss:
+            prob = fit.predict_proba(X.iloc[test, :])
+            score = -1 * scoring(y.iloc[test], prob, sample_weight=sample_weight[test], labels=classifier.classes_)
+        else:
+            pred = fit.predict(X.iloc[test, :])
+            score = scoring(y.iloc[test], pred, sample_weight=sample_weight[test])
+        ret_scores.append(score)
+    return np.array(ret_scores)
+def mean_decrease_impurity(model, feature_names):
+    feature_imp_df = {i: tree.feature_importances_ for i, tree in enumerate(model.estimators_)}
+    feature_imp_df = pd.DataFrame.from_dict(feature_imp_df, orient='index')
+    feature_imp_df.columns = feature_names
+    feature_imp_df = feature_imp_df.replace(0, np.nan)
+
+    importance = pd.concat({'mean': feature_imp_df.mean(),
+                            'std': feature_imp_df.std() * feature_imp_df.shape[0] ** -0.5},
+                           axis=1)
+    importance /= importance['mean'].sum()
+    return importance
+
+def mean_decrease_accuracy(model, X, y, cv_gen, sample_weight = None, scoring = log_loss):
+    if sample_weight is None:
+        sample_weight = np.ones((X.shape[0],))
+
+    fold_metrics_values, features_metrics_values = pd.Series(dtype = float), pd.DataFrame(columns=X.columns)
+
+    for i, (train, test) in enumerate(cv_gen.split(X = X)):
+        fit = model.fit(X=X.iloc[train, :], y=y.iloc[train], sample_weight=sample_weight[train])
+        pred = fit.predict(X.iloc[test, :])
+        if scoring == log_loss:
+            prob = fit.predict_proba(X.iloc[test, :])
+            fold_metrics_values.loc[i] = -scoring(y.iloc[test], prob, sample_weight=sample_weight[test],
+                                                  labels=model.classes_)
+        else:
+            fold_metrics_values.loc[i] = scoring(y.iloc[test], pred, sample_weight=sample_weight[test])
+        for j in X.columns:
+            X1_ = X.iloc[test, :].copy(deep=True)
+            np.random.shuffle(X1_[j].values)
+            if scoring == log_loss:
+                prob = fit.predict_proba(X1_)
+                features_metrics_values.loc[i, j] = -scoring(y.iloc[test], prob, sample_weight=sample_weight[test],
+                                                             labels=model.classes_)
+            else:
+                pred = fit.predict(X1_)
+                features_metrics_values.loc[i, j] = scoring(y.iloc[test], pred,
+                                                            sample_weight=sample_weight[test])
+
+    importance = (- features_metrics_values).add(fold_metrics_values, axis = 0)
+    if scoring == log_loss:
+        importance = importance / -features_metrics_values
+    else:
+        importance = importance / (1.0 - features_metrics_values)
+    importance = pd.concat({'mean': importance.mean(), 'std': importance.std() * importance.shape[0] ** -.5}, axis=1)
+    importance.replace([-np.inf, np.nan], 0, inplace=True)
+
+    return importance
+
+def single_feature_importance(clf, X, y, cv_gen, sample_weight = None, scoring = log_loss):
+    feature_names = X.columns
+    if sample_weight is None:
+        sample_weight = np.ones((X.shape[0],))
+
+    imp = pd.DataFrame(columns = ['mean', 'std'])
+    for feat in feature_names:
+        feat_cross_val_scores = ml_cross_val_score(clf, X = X[[feat]], y = y, sample_weight = sample_weight,
+                                                   scoring = scoring, cv_gen = cv_gen)
+        imp.loc[feat, 'mean'] = feat_cross_val_scores.mean()
+        imp.loc[feat, 'std'] = feat_cross_val_scores.std() * feat_cross_val_scores.shape[0] ** -.5
+    return imp
+
+def probability_weighted_accuracy(y_true, y_pred, probabilities):
+    correct_predictions = (y_true == y_pred)
+    weighted_accuracy = np.sum(correct_predictions * probabilities) / np.sum(probabilities)
+    return weighted_accuracy
+
+def plot_feature_importance(importance_df, oob_score, oos_score, save_fig = False, output_path = None):
+    plt.figure(figsize = (10, importance_df.shape[0] / 5))
+    importance_df.sort_values('mean', ascending = True, inplace = True)
+    importance_df['mean'].plot(kind = 'barh', color = 'b', alpha = 0.25, xerr = importance_df['std'], error_kw = {'ecolor': 'r'})
+    plt.title('Feature importance. OOB Score:{}; OOS score:{}'.format(round(oob_score, 4), round(oos_score, 4)))
+
+    if save_fig is True:
+        plt.savefig(output_path)
+    else:
+        plt.show()
